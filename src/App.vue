c1ac94b22e8986c6e3349a22e2968a13602058e4
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-19 15:28:08 -0500

    part 2 complete

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@ import FlashCardList from './components/FlashCardList.vue'
 </script>
 
 <template>
-  <h1>Star Wars Trivia</h1>
   <div>
-    <FlashCardList />
+    <h1>Star Wars Trivia</h1>
+
+    <div>
+      <FlashCardList />
+    </div>
   </div>
 </template>
 
---
+++ src/components/SelectDifficulty.vue
@@ -0,0 +1,16 @@
+<script setup>
+const emit = defineEmits(['difficulty'])
+
+const selectedDifficulty = (difficulty) => {
+  emit('difficulty', difficulty)
+}
+</script>
+
+<template>
+  <button @click="selectedDifficulty('easy')">Easy</button>
+  <button @click="selectedDifficulty('medium')">Medium</button>
+  <button @click="selectedDifficulty('hard')">Hard</button>
+  <button @click="selectedDifficulty('all')">All</button>
+</template>
+
+<style scoped></style>
