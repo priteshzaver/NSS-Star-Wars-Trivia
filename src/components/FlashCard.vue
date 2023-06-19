4892863e3fd7f6d3102218f45b1a1277f1a5814d
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-19 14:37:09 -0500

    part 1 complete

---
+++ src/components/FlashCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['eachTriviaData'])
+const emit = defineEmits(['flip'])
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div v-if="!props.eachTriviaData.answerShown" class="content">
+        {{ props.eachTriviaData.question }}
+      </div>
+      <div v-else class="content">{{ props.eachTriviaData.answer }}</div>
+    </div>
+    <footer class="card-footer">
+      <button v-if="!props.eachTriviaData.answerShown" @click="emit('flip')">Answer</button>
+      <button v-else @click="emit('flip')">Hide</button>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/FlashCardList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import trivia from '../trivia'
+import FlashCard from './FlashCard.vue'
+
+const triviaData = ref([...trivia])
+</script>
+
+<template>
+  <div class="columns is-multiline">
+    <div v-for="eachTriviaData in triviaData" :key="eachTriviaData.question">
+      <FlashCard
+        :eachTriviaData="eachTriviaData"
+        @flip="eachTriviaData.answerShown = !eachTriviaData.answerShown"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
